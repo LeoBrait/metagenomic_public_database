bd3914e9256bd60024f502de11a8c55037b54fd1
Leonardo Brait <leonardobrait@gmail.com>
2023-02-27 20:56:02 -0300

    salved some problems in categorization

--- src/main.R
+++ src/main.R
@@ -1,44 +1,57 @@
 library("tidyverse")
 
-metadata <- read.csv("raw_data//27022023//mg-rast_metadata.csv", sep = ";")
-project_id <- read.csv("raw_data//27022023//metadata_complete.csv")
+metadata_raw <- read.csv("raw_data//27022023//mg-rast_metadata.csv", sep = ";")
+metadata_issued <- read.csv("raw_data//27022023//metadata_complete.csv")
 
-metadata <- metadata %>%
-select(sample,
-        biome,
-        feature,
-        material,
-        env.package,
-        metagenome_taxonomy,
-        sample_name,
-        project_name,
-        investigation_type,
-        sequence_type,
-        continent,
-        country,
-        latitude,
-        longitude)
+metadata_raw <- metadata_raw %>%
+  select(
+    sample,
+    biome,
+    feature,
+    material,
+    env.package,
+    metagenome_taxonomy,
+    sample_name,
+    project_name,
+    investigation_type,
+    sequence_type,
+    continent,
+    country,
+    latitude,
+    longitude) %>%
+  mutate(raw_biome = biome) %>%
+  select(-biome)
 
-project_id <- project_id %>%
-select(samples, project_id)
+metadata_issued <- metadata_issued %>%
+  select(
+    samples,
+    project_id,
+    biome,
+    life_style,
+    environment,
+    habitat) %>%
+  mutate(
+    biosphere = biome,
+    ecosystem = environment) %>%
+  select(-biome, -environment)
 
 #change column name from sample to samples
-colnames(metadata)[1] <- "samples"
+colnames(metadata_raw)[1] <- "samples"
+metadata_merged <- left_join(metadata_raw, metadata_issued, by = "samples")
 
-metadata <- inner_join(metadata, project_id, by = "samples")
+write.csv(
+  metadata_merged,
+  "treated_data//27022023//base_table.csv", row.names = FALSE)
 
-write.csv(metadata, "treated_data//27022023//base_table.csv", row.names = FALSE)
+metadata_merged$raw_biome <- gsub("/", "_slash_", metadata_merged$raw_biome)
+metadata_merged$raw_biome <- as.factor(metadata_merged$raw_biome)
 
-metadata$biome <- gsub("/", "_slash_", metadata$biome)
-metadata$biome <- as.factor(metadata$biome)
-
-biomes <- levels(metadata$biome)
-#replace / character with _
+raw_biomes <- levels(metadata_merged$raw_biome)
 
 
-for(x in 1:length(biomes)){
-  subset <- subset(metadata, biome == biomes[x])
+for(x in 1:length(raw_biomes)){
+  subset <- subset(metadata_merged, raw_biome == raw_biomes[x])
   write.csv(
     subset, paste0("treated_data//27022023//biome_tables//",
-    biomes[x], ".csv"), row.names = FALSE)
+    raw_biomes[x], ".csv"), row.names = FALSE)
 }
