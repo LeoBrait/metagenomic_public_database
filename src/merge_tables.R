02fb879f05257b832e775cfda3f0ffe955cdccc4
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-03-04 17:00:07 -0300

    merged the curated dataframes into one dataframe it is in the folder 04_curated_metadata the script is in the scr folder

--- src/merge_tables.R
+++ src/merge_tables.R
@@ -3,26 +3,32 @@
 
 #Libraries#
 library(tidyverse)
+library(data.table)
 
-#Creating a list with the file names
-#in each folder of the "biome_tables_organized" folder.#
+#Creating a list with the file names in each folder
+#of the "biome_tables_organized" folder.
 #for this, the function list.files was used,
-#with the argument pattern = ".csv" to exlude the readme files.#
-dflist <- list.files(path = "data\\03_manual_treatment\\biome_tables_organized",
-         pattern = ".csv", full.names = FALSE,
-         recursive = TRUE, include.dirs = FALSE)
+#with the argument pattern = "*.csv" to exlude the readme files,
+#the argument recursive = TRUE because the datasets are in subfolders,
+#and the argument full.names = TRUE to get the full directory.#
+files <- list.files(path = "data\\03_manual_treatment\\biome_tables_organized",
+         pattern = "*.csv", full.names = TRUE, recursive = TRUE)
 
-View(dflist) #Viewing the list.
+View(files) #Viewing the list.
 
-#Now, geting the number of dataframes in the list with the function length.#
-n_of_datasets <- length(dflist)
+#Taking the the files list and applying the function fread
+#in each item with lapply function, and calling rbind to all
+#the outputs of the lapply function, i.e. the separate dataframes
+#joining all of them into one final dataset.#
+df <- do.call(rbind, lapply(files, fread))
 
-print(n_of_datasets) #Printing the number.
+#Coercing to a dataframe
+df <- as.data.frame(unclass(df))
 
-#Now lets use a for loop to load every dataframe in those folders.#
-for (item in dflist){
-    for (i in 1:n_of_datasets){
-        df_[i] <- read.csv(
-            file = "data\\03_manual_treatment\\biome_tables_organized\\", item)
-    }
-}
+View(df)
+
+d <- Sys.Date() %>% str_replace_all("-", "_")
+write.csv(df, 
+    file = paste(
+        "data\\04_metadata_after_curatory\\curated_metadata_", d, ".csv"
+        ))
