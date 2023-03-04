0b901932cce73ca0fbd9d51f5a7545dc19e4fa01
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-03-04 12:47:20 -0300

    WIP

--- src/merge_tables.R
+++ src/merge_tables.R
@@ -4,10 +4,13 @@
 #Libraries#
 library(tidyverse)
 
-#Creating a list with the file names in each folder of the "biome_tables_organized" folder.#
-#for this, the function list.files was used, with the argument pattern = ".csv" to exlude the readme files.#
-dflist <- list.files(path = "data/03_manual_treatment/biome_tables_organized", pattern = ".csv",
-                     full.names = FALSE, recursive = TRUE, include.dirs = FALSE)
+#Creating a list with the file names
+#in each folder of the "biome_tables_organized" folder.#
+#for this, the function list.files was used,
+#with the argument pattern = ".csv" to exlude the readme files.#
+dflist <- list.files(path = "data\\03_manual_treatment\\biome_tables_organized",
+         pattern = ".csv", full.names = FALSE,
+         recursive = TRUE, include.dirs = FALSE)
 
 View(dflist) #Viewing the list.
 
@@ -17,3 +20,9 @@ n_of_datasets <- length(dflist)
 print(n_of_datasets) #Printing the number.
 
 #Now lets use a for loop to load every dataframe in those folders.#
+for (item in dflist){
+    for (i in 1:n_of_datasets){
+        df_[i] <- read.csv(
+            file = "data\\03_manual_treatment\\biome_tables_organized\\", item)
+    }
+}
