c238aa7ab2ac7a4d6bf18980919a6b88ad39d253
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-03-23 20:02:43 -0300

    WIP

--- src/mesuring_dispersion.R
+++ src/mesuring_dispersion.R
@@ -12,6 +12,15 @@ data_after <- read.csv(
     "data/06_taxonomic_table/phyla_relative_matrix_after_curatory_and_IUCN_classification.csv", 
     header = TRUE)
 
+#converting fake char columns to numeric columns
+for(i in 9:length(data_after)){
+    data_after[i] <- as.numeric(data_after[[i]])
+    data_after[is.na(data_after)] <- 0
+}
+str(data_after)
+
+is.na(data_after) %>% sum() # chacking for na values
+
 categories_final <- data_after[,2:8] 
 categories_before <- data_before[,2:4]
 #Viewing total sample numver in tables
@@ -32,25 +41,42 @@ dist_before <- data_before %>%
     vegdist(,method = "bray")
 
 dist_after <- data_after %>%
-    select(-life_style, -biosphere, -ecosystem, -habitat, -Realm., -Biome, -Functional.group) %>%
+    select(-life_style, -biosphere, -ecosystem, -habitat, -Realm, -Biome, -Functional_group) %>%
     column_to_rownames("samples") %>%
-    select_if(is.numeric) %>%
     vegdist(,method = "bray")
-
-#calculating nmds
-set.seed(1)
-nmds_before <- metaMDS(dist_before)
-
 
 
 #calculating dispersion from data_before
-#getting the group
-habitat_before <- data_before$habitat
-habitat_before <- as.factor(habitat_before)
-dispersion_before <- betadisper(dist_before, group = habitat_before, type = "centroid")
+dispersion_before <- betadisper(dist_before, group = data_before$level_2, type = "centroid")
 
 #calculating dispersion from data_after
-#getting the group
-habitat_after <- data_after$habitat
-habitat_after <- as.factor(habitat_after)
-dispersion_after <- betadisper(dist_after, group = habitat_after, type = "centroid")
+dispersion_after <- betadisper(dist_after, group = data_after$ecosystem, type = "centroid")
+
+#plotting dispersion
+dispersion_plot_before <- boxplot(dispersion_before$distances ~ dispersion_before$group,
+                          main = "Dispersion of samples before curatory",
+                          xlab = "Ecosystems",
+                          ylab = "Dispersion",
+                          col = "blue",
+                          #horizontal = TRUE,
+                          las = 2,
+                          cex.lab = 1.5,
+                          cex.axis = 1.5,
+                          cex.main = 1.5,
+                          cex.sub = 1.5,
+                          cex = 1.5)
+
+dev.print(dispersion_plot_before, file = "output/01_dispersion/boxplot_before_plot.png")
+
+after_plot <- boxplot(dispersion_after$distances ~ dispersion_after$group,
+                          main = "Dispersion of samples before curatory",
+                          xlab = "Ecosystems",
+                          ylab = "Dispersion",
+                          col = "blue",
+                          #horizontal = TRUE,
+                          las = 2,
+                          cex.lab = 1.5,
+                          cex.axis = 1.5,
+                          cex.main = 1.5,
+                          cex.sub = 1.5,
+                          cex = 1.5)
