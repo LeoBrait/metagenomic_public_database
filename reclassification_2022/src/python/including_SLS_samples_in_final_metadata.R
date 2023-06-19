d40add0ddd7f43632524d328fccdcaa9df24a59f
diogoburgos <110135811+diogoburgos@users.noreply.github.com>
2023-06-19 14:57:33 -0300

    finish creating final_IUCN_classification

--- reclassification_2022/src/python/including_SLS_samples_in_final_metadata.R
+++ reclassification_2022/src/python/including_SLS_samples_in_final_metadata.R
@@ -9,7 +9,7 @@ sls_samples
 sls_samples_df <- df %>% filter(samples %in% sls_samples)
 View(sls_samples_df)
 
-df <- read_csv("final_metadata/final_IUCN_classification.csv")
+df <- read_csv("final_metadata/IUCN_classification_without_sls_samples.csv")
 
 df <- df %>%
     subset(select = -c(metagenome_taxonomy, investigation_type))
@@ -30,4 +30,10 @@ sls_samples_df[c("Biome")] <- "Subterranean_freshwaters"
 str(sls_samples_df)
 
 final_df <- rbind(df, sls_samples_df)
+
+final_df <- final_df %>%
+    subset(select = c(samples, Realm, Biome, Functional_group))
+
 View(final_df)
+
+write_csv(final_df, "final_metadata/final_IUCN_classification.csv")
