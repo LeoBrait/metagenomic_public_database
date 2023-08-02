c071ca7c7f5e0fe0eb83a9123a765895478b8e13
Leonardo Brait <leonardobrait@gmail.com>
2023-08-02 12:54:25 -0300

    rename of.r extension

---
+++ R/deprecated/get_seq_method_.r
@@ -0,0 +1,26 @@
+library("tidyverse")
+
+genomic <- read_csv2(
+    "D:/documentos/leonardo/01_universidade/01_biome_lab/1_paper_production/metagenomic_public_database/genomic_content_summaries/merged_metadata.csv" 
+
+)
+
+metadata_complete <- read_csv(
+    "D:/documentos/leonardo/01_universidade/01_biome_lab/1_paper_production/metagenomic_public_database/genomic_content_summaries/metadata_complete.csv"
+)
+
+metadata_complete <- metadata_complete %>%
+ select(samples, seq_meth )
+
+merged_table <- genomic %>%
+  inner_join(metadata_complete, by = "samples")
+unique(merged_table$seq_meth)
+write.csv(
+    merged_table,
+    file = paste0(
+        "D:/documentos/leonardo/01_universidade/01_biome_lab/",
+        "1_paper_production/metagenomic_public_database/",
+        "genomic_content_summaries/genomic_content.csv"
+    ),
+    row.names = FALSE
+)
